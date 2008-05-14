236c11710ba18ce11392858579f59962cd2244af
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-05-14 16:04:40 +0000

    files are still needed and restored again
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/graytree/branches/Version-0-1-X@9165 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ mod_cmd/conf.php
@@ -0,0 +1,22 @@
+<?php
+$path = realpath($_SERVER['SCRIPT_PATH']);
+if (strstr($path, 'typo3conf') !== false) 	{
+	define('TYPO3_MOD_PATH', '../typo3conf/ext/graytree/mod_cmd/');
+	$BACK_PATH='../../../../typo3/';
+} else {
+	define('TYPO3_MOD_PATH', 'ext/graytree/mod_cmd/');
+	$BACK_PATH='../../../';
+}
+
+$MCONF['name']='web_txgraytreeCmd';
+$MCONF['access']='';
+
+$MCONF['exclude']=TRUE;
+$MCONF['shy']=TRUE;
+
+$MCONF['script']='index.php';
+
+$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
+$MLANG['default']['ll_ref']='LLL:EXT:graytree/mod_cmd/locallang_mod.php';
+
+?>
